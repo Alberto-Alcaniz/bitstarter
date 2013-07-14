4fecf30df323b78d32292c318b14b0b41603e504
Alberto-Alcaniz <example@domain.com>
2013-07-14 01:43:49 +0000

    trying fs within buffer

--- web.js
+++ web.js
@@ -2,14 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var fs = require('fs');
+var fs = require('fs');
 
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-    var buffer = new Buffer( 256 ); 
+    var buffer = new Buffer( fs.readFileSync('index.html') ); 
 //    buffer.write( fs.readFileSync('index.html') );
-    buffer.write('Hellor World buffer!');
+//    buffer.write('Hellor World buffer!');
     response.send( buffer.toString('utf8') );
 //    response.send('Hello World2!');
 });
