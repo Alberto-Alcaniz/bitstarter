e0661781817231822e01e43f4047b3883cf186b7
Alberto-Alcaniz <example@domain.com>
2013-07-14 00:42:07 +0000

    testing

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
     var buffer = new Buffer( 25 ); 
-    var buffer.write( fs.readFileSync('index.html') );
+//    var buffer.write( fs.readFileSync('index.html') );
 
 //    response.send( buffer.toString('utf8', 0, 25) );
     response.send('Hello World2!');
